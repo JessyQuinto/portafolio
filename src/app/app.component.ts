e4a671f22d680eeebcd5746ad56ac32951ab90d5
JessyQT <60488881+JessyQuinto@users.noreply.github.com>
2024-06-14 21:08:01 -0500

    UPDATE
    Se instala Flow bite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Corrección de 'styleUrl' a 'styleUrls'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portafolio';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      initFlowbite();
+
+      document.documentElement.classList.add('dark');
+    }
+  }
 }
